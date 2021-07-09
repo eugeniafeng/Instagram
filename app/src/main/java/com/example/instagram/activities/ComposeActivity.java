ab46f9de0964138e39bbe4f2e3a9f6485ee89036
eugeniafeng <eugeniafeng@gmail.com>
2021-07-09 16:34:17 -0400

    Added bottom navigation bar with Instagram icons

--- app/src/main/java/com/example/instagram/activities/ComposeActivity.java
+++ app/src/main/java/com/example/instagram/activities/ComposeActivity.java
@@ -3,6 +3,7 @@ package com.example.instagram.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -14,12 +15,15 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.instagram.R;
 import com.example.instagram.utils.BitmapScaler;
 import com.example.instagram.databinding.ActivityComposeBinding;
 import com.example.instagram.models.Post;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 
@@ -75,6 +79,23 @@ public class ComposeActivity extends AppCompatActivity {
             }
             ParseUser currentUser = ParseUser.getCurrentUser();
             savePost(description, currentUser, photoFile);
+        });
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.action_home:
+                    Toast.makeText(ComposeActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_compose:
+                    Toast.makeText(ComposeActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_profile:
+                default:
+                    Toast.makeText(ComposeActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
         });
     }
 
